a05da89ccb3fa77d5803382cfa5f9f96a767aff3
virtual-life <pavithra03@gmail.com>
2016-08-01 08:47:23 -0700

    Update product-of-array-except-self.java

--- questions/product-of-array-except-self.java
+++ questions/product-of-array-except-self.java
@@ -37,4 +37,32 @@ public class Solution {
 
         return result;
     }
+    
+    
+    public int[] getProductsOfAllIntsExceptAtIndex(int[] intArray) {
+
+    // we make an array with the length of the input array to
+    // hold our products
+    int[] productsOfAllIntsExceptAtIndex = new int[intArray.length];
+
+    // for each integer, we find the product of all the integers
+    // before it, storing the total product so far each time
+    int productSoFar = 1;
+    for (int i = 0; i < intArray.length; i++) {
+        productsOfAllIntsExceptAtIndex[i] = productSoFar;
+        productSoFar *= intArray[i];
+    }
+
+    // for each integer, we find the product of all the integers
+    // after it. since each index in products already has the
+    // product of all the integers before it, now we're storing
+    // the total product of all other integers
+    productSoFar = 1;
+    for (int i = intArray.length - 1; i >= 0; i--) {
+        productsOfAllIntsExceptAtIndex[i] *= productSoFar;
+        productSoFar *= intArray[i];
+    }
+
+    return productsOfAllIntsExceptAtIndex;
+}
 }
