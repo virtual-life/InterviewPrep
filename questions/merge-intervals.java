d6194a3f14ca0790a49995e9be4ad74135ddc7b1
virtual-life <pavithra03@gmail.com>
2016-08-01 13:31:53 -0700

    Update merge-intervals.java

--- questions/merge-intervals.java
+++ questions/merge-intervals.java
@@ -1,3 +1,5 @@
+// Time - O(nlogn)
+// Space - O(n)
 public class Solution {
     public List<Interval> merge(List<Interval> intervals) {
 
@@ -31,4 +33,35 @@ class IntervalComparator implements Comparator<Interval>{
     public int compare(Interval v1, Interval v2){
         return v1.start - v2.start;
     }
+}
+
+public List<Meeting> mergeRanges(List<Meeting> meetings) {
+
+    // sort by start times
+    List<Meeting> sortedMeetings = new ArrayList<Meeting>(meetings);
+    Collections.sort(sortedMeetings, new Comparator<Meeting>() {
+        public int compare(Meeting m1, Meeting m2)  {
+            return m1.startTime - m2.startTime;
+        }
+    });
+
+    // initialize mergedMeetings with the earliest meeting
+    List<Meeting> mergedMeetings = new ArrayList<Meeting>();
+    mergedMeetings.add(sortedMeetings.get(0));
+
+    for (Meeting currentMeeting : sortedMeetings) {
+
+        Meeting lastMergedMeeting = mergedMeetings.get(mergedMeetings.size() - 1);
+
+        // if the current and last meetings overlap, use the latest end time
+        if (currentMeeting.startTime <= lastMergedMeeting.endTime) {
+            lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+
+        // add the current meeting since it doesn't overlap
+        } else {
+            mergedMeetings.add(currentMeeting);
+        }
+    }
+
+    return mergedMeetings;
 }
