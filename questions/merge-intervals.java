b0ca2a638b6aaacfaa184679346b55912d39b9ea
virtual-life <pavithra03@gmail.com>
2016-08-02 16:14:24 -0700

    Update merge-intervals.java

--- questions/merge-intervals.java
+++ questions/merge-intervals.java
@@ -65,3 +65,8 @@ public List<Meeting> mergeRanges(List<Meeting> meetings) {
 
     return mergedMeetings;
 }
+
+/**
+    What if we did have an upper bound on the input values? Could we improve our runtime? Would it cost us memory?
+    Could we do this "in-place" on the input list and save some space? What are the pros and cons of doing this in-place?
+ * /
