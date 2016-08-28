bbb9ae5e800f5ac403fc353134389e6ce4056fb5
virtual-life <pavithra03@gmail.com>
2016-08-28 14:43:47 -0700

    Update reverse-words-string.java

--- questions/reverse-words-string.java
+++ questions/reverse-words-string.java
@@ -18,6 +18,10 @@ class SolutionRS {
 
     /**
      *  In Place
+     
+     a) Reverse all the characters in the entire message, giving us the correct word order but with each word backwards.
+     b) Reverse the characters in each individual word.
+
      * Time O(n) Space O(n)
      */
     public char[] reverseWordsInPlace(char[] s ) {
