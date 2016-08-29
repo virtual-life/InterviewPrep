dbecc0ed8fd7a6e6cbbc1369723695e8be2aa191
virtual-life <pavithra03@gmail.com>
2016-08-29 12:54:17 -0700

    Update max-product-subarray.java

--- questions/dynamic-programming/max-product-subarray.java
+++ questions/dynamic-programming/max-product-subarray.java
@@ -2,6 +2,8 @@
  Find the contiguous subarray within an array (containing at least one number) which has the largest product.
 
  For example, given the array [2,3,-2,4], the contiguous subarray [2,3] has the largest product = 6.
+ 
+ Time O(n) Space O(1)
  */
 
 public class Solution {
