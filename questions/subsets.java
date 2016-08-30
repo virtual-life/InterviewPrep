169c4d8a642d636852ecdd3079acc57a0c33dad7
virtual-life <pavithra03@gmail.com>
2016-08-30 04:58:09 -0700

    Update subsets.java

--- questions/subsets.java
+++ questions/subsets.java
@@ -1,3 +1,24 @@
+/**
+ Given a set of distinct integers, nums, return all possible subsets.
+
+Note: The solution set must not contain duplicate subsets.
+
+For example,
+If nums = [1,2,3], a solution is:
+
+[
+  [3],
+  [1],
+  [2],
+  [1,2,3],
+  [1,3],
+  [2,3],
+  [1,2],
+  []
+]
+*/
+
+
 /** Time - O(2^n) */
 public class Solution{
     public ArrayList<ArrayList<Integer>> subsets(int[] S) {
