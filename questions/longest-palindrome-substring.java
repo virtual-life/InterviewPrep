929afe9f51f4c038a643c509ec47aa42694a27b0
virtual-life <pavithra03@gmail.com>
2016-08-29 17:33:14 -0700

    Update longest-palindrome-substring.java

--- questions/longest-palindrome-substring.java
+++ questions/longest-palindrome-substring.java
@@ -4,6 +4,14 @@
  You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
 
  Time O(n^2), Space O(1)
+ 
+ We observe that a palindrome mirrors around its center. 
+ Therefore, a palindrome can be expanded from its center, and there are only 2N-1 such centers.
+
+You might be asking why there are 2N-1 but not N centers?
+The reason is the center of a palindrome can be in between two letters. Such palindromes have even number of letters (such as “abba”) and its center are between the two ‘b’s.
+
+Since expanding a palindrome around its center could take O(N) time, the overall complexity is O(N2).
 
  */
 
@@ -19,7 +27,7 @@ public class Solution {
         }
 
         String longest = s.substring(0, 1);
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length()-1 ; i++) {
             // get longest palindrome with center of i
             //for odd length
             String tmp = helper(s, i, i);
@@ -45,7 +53,7 @@ public class Solution {
             begin--;
             end++;
         }
-        return s.substring(begin+1, end);
+        return s.substring(begin+1, end-1);
     }
 
 }
