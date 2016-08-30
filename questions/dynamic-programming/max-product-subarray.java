77e752f0542922d7588920bd8edc72553c64305b
virtual-life <pavithra03@gmail.com>
2016-08-29 17:05:22 -0700

    Update max-product-subarray.java

--- questions/dynamic-programming/max-product-subarray.java
+++ questions/dynamic-programming/max-product-subarray.java
@@ -5,6 +5,39 @@
  
  Time O(n) Space O(1)
  */
+ 
+ 
+ /*
+ You have three choices to make at any position in array.
+1. You can get maximum product by multiplying the current element with 
+    maximum product calculated so far.  (might work when current 
+    element is positive).
+2. You can get maximum product by multiplying the current element with 
+    minimum product calculated so far. (might work when current 
+    element is negative).
+3.  Current element might be a starting position for maximum product sub
+     array
+     
+ curr_max_prod = A[0];
+prev_max_prod = A[0];
+prev_min_prod = A[0];
+ans = A[0];
+for i=1  to  n-1
+{
+     curr_max_prod = MAX ( prev_max_prod*A[i],  
+                                                 prev_min_prod*A[i], 
+                                                 A[i] );
+
+     curr_min_prod = MIN ( prev_max_prod*A[i],  
+                                                 prev_min_prod*A[i], 
+                                                 A[i] );
+     Ans = MAX(ans, curr_max_prod);
+     prev_max_prod = curr_max_prod;
+     prev_min_prod = curr_min_prod;
+}
+return ans;
+
+ */
 
 public class Solution {
     public int maxProduct(int[] nums) {
@@ -12,15 +45,17 @@ public class Solution {
             return 0;
         }
         int len = nums.length;
+        // min negative product ending at the current position
         int min = nums[0];
+        // max positive product ending at the current position
         int max = nums[0];
-
+        // Overall max product
         int result = nums[0];
 
         for (int i = 1; i < len; i++) {
 
-            int curMin = Math.min(
-                    Math.min(min * nums[i], max * nums[i]),
+            int curMin = Math.min( 
+                    Math.min(max * nums[i], min * nums[i]), 
                     nums[i]);
             int curMax = Math.max(
                     Math.max(max * nums[i], min * nums[i]),
