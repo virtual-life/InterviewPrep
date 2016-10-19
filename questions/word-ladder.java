5bf8f76c5a6e931a3b76ae0a66c8572bedfc6824
virtual-life <pavithra03@gmail.com>
2016-10-19 14:35:15 -0700

    Update word-ladder.java

--- questions/word-ladder.java
+++ questions/word-ladder.java
@@ -79,6 +79,9 @@ public class Solution {
     /**
      To track the actual ladder, we need to add a pointer that points to the previous node in the WordNode class
      In addition, the used word can not directly removed from the dictionary. The used word is only removed when steps change.
+     
+    If we find one path using the BFS, then all the other shortest paths must also in this level, so the search will stop once this level ends.
+
 
      */
 
