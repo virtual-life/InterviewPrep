4ba02921051cdb3f17d54e9266a5f9bf0f201fb0
virtual-life <pavithra03@gmail.com>
2016-10-19 17:45:38 -0700

    Update validate-binary-search-tree.java

--- questions/validate-binary-search-tree.java
+++ questions/validate-binary-search-tree.java
@@ -20,7 +20,7 @@ public class Solution {
             return isValidBST(root.left, min, root.val) && isValidBST(root.right, root.val, max);
     }
 
-
+}
     /**
      If the violation occurs close to the root but on the right subtree, the above method still cost O(n).
      Iterative solution below can handle violations close to root node faster. -
@@ -29,9 +29,41 @@ public class Solution {
 
      */
 
-
-
-
-
+  
+public class Solution {
+    public boolean isValidBST(TreeNode root) {
+        if(root == null)
+            return true;
+ 
+        LinkedList<BNode> queue = new LinkedList<BNode>();
+        queue.add(new BNode(root, Double.NEGATIVE_INFINITY, Double.POSITIVE_INFINITY));
+        while(!queue.isEmpty()){
+            BNode b = queue.poll();
+            if(b.n.val <= b.left || b.n.val >=b.right){
+                return false;
+            }
+            if(b.n.left!=null){
+                queue.offer(new BNode(b.n.left, b.left, b.n.val));
+            }
+            if(b.n.right!=null){
+                queue.offer(new BNode(b.n.right, b.n.val, b.right));
+            }
+        }
+        return true;
+    }
 }
+//define a BNode class with TreeNode and it's boundaries
+class BNode{
+    TreeNode n;
+    double left;
+    double right;
+    public BNode(TreeNode n, double left, double right){
+        this.n = n;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+
+
 
