8e8d41f9e1bf3f731b15e3dba3241fa80e9110a5
virtual-life <pavithra03@gmail.com>
2016-12-09 06:42:03 -0800

    Create gcd.java

---
+++ questions/gcd.java
@@ -0,0 +1,21 @@
+//gcd of two nums 
+
+//A recursive method would be:
+
+static int gcd(int a, int b)
+{
+  if(a == 0 || b == 0) return a+b; // base case
+  return gcd(b,a%b);
+}
+Using a while loop:
+
+static int gcd(int a, int b)
+{
+  while(a!=0 && b!=0) // until either one of them is 0
+  {
+     int c = b;
+     b = a%b;
+     a = c;
+  }
+  return a+b; // either one is 0, so return the non-zero value
+}
