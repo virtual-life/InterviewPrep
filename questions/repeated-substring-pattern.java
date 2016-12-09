a6016422ad5fb72ff250fe58830a66376902192b
virtual-life <pavithra03@gmail.com>
2016-12-09 05:38:04 -0800

    Create repeated-substring-pattern.java

---
+++ questions/repeated-substring-pattern.java
@@ -0,0 +1,56 @@
+/**
+Given a non-empty string check if it can be constructed by taking a substring of it and appending multiple copies of the substring together. You may assume the given string consists of lowercase English letters only and its length will not exceed 10000.
+
+Example 1:
+Input: "abab"
+
+Output: True
+
+Explanation: It's the substring "ab" twice.
+Example 2:
+Input: "aba"
+
+Output: False
+Example 3:
+Input: "abcabcabcabc"
+
+Output: True
+
+Explanation: It's the substring "abc" four times. (And the substring "abcabc" twice.)
+
+Time - O(
+*/
+
+/**
+Roughly speaking, dp[i+1] stores the maximum number of characters that the string is repeating itself up to position i.
+Therefore, if a string repeats a length 5 substring 4 times, then the last entry would be of value 15.
+To check if the string is repeating itself, we just need the last entry to be non-zero and str.size() to divide (str.size()-last entry).
+
+The one-bit array maintained by dp [i] indicates the number of characters in the repeated string up to position i-1, 
+excluding the string that is repeated. What is the meaning of this, for example, "abcabc"? Dp array is [0 0 0 0 1 2 3], 
+dp array length is one more than the original string length. 
+Then we look at the last position of the number 3, it means that the number of repeated string of three characters. 
+If it is "abcabcabc", then dp array [0 0 0 0 1 2 3 4 5 6], we find that the last number is 6, then that the string is "abcabc", 
+6 characters. So how the last number to know whether the original string is composed of repeated substrings, first of all, 
+of course, is the last number can not be 0, but also to meet dp [n]% (n - dp [n]) = = 0, 
+because n - dp [n] is the length of a substring, the length of the repeated string is definitely an integer multiple of a substring,
+*/
+
+public class Solution {
+    public boolean repeatedSubstringPattern(String str) {
+        int n = str.length(), i = 1, j = 0; 
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        char [] s = str.toCharArray();
+        while( i<n ) {
+            if( s[i] == s[j] ) {
+                dp[++i]=++j;
+            }
+            else if( j == 0 ) 
+                i++;
+            else 
+                j = dp[j];
+        }
+        return (dp[n] >0  && dp[n]%(n-dp[n])==0);
+    }
+}
