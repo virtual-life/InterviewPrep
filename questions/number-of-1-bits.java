e31cab5640c2f2d5b12c6f29e26a728f37337f33
virtual-life <pavithra03@gmail.com>
2016-12-09 06:26:07 -0800

    Update number-of-1-bits.java

--- questions/number-of-1-bits.java
+++ questions/number-of-1-bits.java
@@ -11,10 +11,13 @@
 
 public class Solution {
     // you need to treat n as an unsigned value
-    public int hammingWeight(int n) {
+    public int hammingWeight(long n) {
         int count = 0;
         for (int i = 0; i < 32; i++) {
-            count = count + (n & 1);
+            if ((n & 1) == 1){
+                count++;
+            }
+            // divide by 2 
             n = n >> 1;
         }
 
