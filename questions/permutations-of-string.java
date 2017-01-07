052ac4e73adbe63e1f34adbcc7497dc11a313ce0
virtual-life <pavithra03@gmail.com>
2017-01-07 12:46:47 -0800

    Create permutations-of-string.java

---
+++ questions/permutations-of-string.java
@@ -0,0 +1,41 @@
+/*
+Time - O(n!)
+Space - 
+*/
+
+// Recursion 
+
+public Set<String> getPermutations(String inputString) {
+
+    // base case
+    if (inputString.length() <= 1) {
+        return new HashSet<String>(Arrays.asList(inputString));
+    }
+
+    String allCharsExceptLast = inputString.substring(0, inputString.length() - 1);
+    char lastChar = inputString.charAt(inputString.length() - 1);
+
+    // recursive call: get all possible permutations for all chars except last
+    Set<String> permutationsOfAllCharsExceptLast = getPermutations(allCharsExceptLast);
+
+    // put the last char in all possible positions for each of the above permutations
+    Set<String> permutations = new HashSet<String>();
+    for (String permutationOfAllCharsExceptLast : permutationsOfAllCharsExceptLast) {
+        for (int position = 0; position <= allCharsExceptLast.length(); position++) {
+            String permutation = permutationOfAllCharsExceptLast.substring(0, position) + lastChar + permutationOfAllCharsExceptLast.substring(position);
+            permutations.add(permutation);
+        }
+    }
+
+    return permutations;
+}
+
+
+//Iterative 
+
+
+
+/*
+Bonus
+What if our input string had some duplicates?
+*/
