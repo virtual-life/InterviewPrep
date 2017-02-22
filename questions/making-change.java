4c8ba5440672aaad5846a6f7bfdf8116664043c5
virtual-life <pavithra03@gmail.com>
2017-02-22 14:27:30 -0800

    Update making-change.java

--- questions/making-change.java
+++ questions/making-change.java
@@ -15,5 +15,22 @@ Example: for amount=44 (44¢) and denominations=[1,2,3][1,2,3] (11¢, 22¢ and 3
 1¢, 3¢
 2¢, 2¢
 
+Time -  O(n*m) n -  amount of money &  m -  number of denominations.
+Space - O(n)  
 
 */
+
+  public int changePossibilitiesBottomUp(int amount, int[] denominations) {
+    int[] waysOfDoingNCents = new int[amount+1]; // array of zeros from 0..amount
+    waysOfDoingNCents[0] = 1;
+
+    for (int coin : denominations) {
+        for (int higherAmount = coin; higherAmount < amount + 1; higherAmount++) {
+            int higherAmountRemainder = higherAmount - coin;
+            waysOfDoingNCents[higherAmount] += waysOfDoingNCents[higherAmountRemainder];
+        }
+    }
+
+    return waysOfDoingNCents[amount];
+}
+
