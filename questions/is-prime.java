2b99bd305477135a9c6c977eb56ab5fee7e42f75
virtual-life <pavithra03@gmail.com>
2017-03-05 11:11:47 -0800

    Update is-prime.java

--- questions/is-prime.java
+++ questions/is-prime.java
@@ -4,11 +4,11 @@ public class Solution {
 	public int isPrime(int A) {
 	    if (A < 2) 
 	        return 0;
-		int upperLimit = (int)(Math.sqrt(A));
-		for (int i = 2; i <= upperLimit; i++) {
-			if (i < A && A % i == 0) 
-			return 0;
-		}
-		return 1;
+	    int upperLimit = (int)(Math.sqrt(A));
+	    for (int i = 2; i <= upperLimit; i++) {
+		if (i < A && A % i == 0) 
+		     return 0;
+	    }
+	    return 1;
 	}
 }
