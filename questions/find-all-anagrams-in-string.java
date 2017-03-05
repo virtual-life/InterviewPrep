eb92498feac538eaaefb4e68ed50d2953a3af3f9
virtual-life <pavithra03@gmail.com>
2017-03-05 09:42:32 -0800

    Update find-all-anagrams-in-string.java

--- questions/find-all-anagrams-in-string.java
+++ questions/find-all-anagrams-in-string.java
@@ -30,35 +30,31 @@ Time Complexity will be O(n)
 public class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-      int[] chars = new int[26];
-      List<Integer> result = new ArrayList<>();
+    int[] chars = new int[26];
+    List<Integer> result = new ArrayList<>();
 
-      if (s == null || p == null || s.length() < p.length())
+    if (s == null || p == null || s.length() < p.length())
         return result;
-      for (char c : p.toCharArray())
+    for (char c : p.toCharArray())
         chars[c-'a']++;
 
-      int start = 0, end = 0, count = p.length();
-      // Go over the string
-      while (end < s.length()) {
-        // If the char at start appeared in p, we increase count
-        if (end - start == p.length() && chars[s.charAt(start)-'a']++ >= 0){
-            //chars[s.charAt(start)-'a']++;
-            start++;
-            count++;
-        }    
-            
+    int start = 0, end = 0, count = p.length();
+    // Go over the string
+    while (end < s.length()) {
+        
         // If the char at end appeared in p (since it's not -1 after decreasing), we decrease count
-        if (--chars[s.charAt(end)-'a'] >= 0){
-             end++;
-             count--;
-        }    
-           
+        if (chars[s.charAt(end++)-'a']-- >= 1)
+            count--;
+        
         if (count == 0)
             result.add(start);
-      }
+        
+        // If the char at start appeared in p, we increase count
+        if (end - start == p.length() && chars[s.charAt(start++)-'a']++ >= 0)
+            count++;
+    }
     
-      return result;
+    return result;
         
     }
 }
