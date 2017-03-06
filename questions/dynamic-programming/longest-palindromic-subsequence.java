b10b9822225f4366b2e0fc10e483d6e36f7f0b11
virtual-life <pavithra03@gmail.com>
2017-03-06 12:10:23 -0800

    Update longest-palindromic-subsequence.java

--- questions/dynamic-programming/longest-palindromic-subsequence.java
+++ questions/dynamic-programming/longest-palindromic-subsequence.java
@@ -18,6 +18,26 @@ One possible longest palindromic subsequence is "bb".
 
 Time - O(n^2)
 Space - O(n^2)
+http://algorithms.tutorialhorizon.com/longest-palindromic-subsequence/
+
+*/
+
+/**
+Check the first and the last characters of the sequence. Now there are two possibilities, either both the characters same or distinct. 
+We will have to handle both the case.
+
+If both characters are same, add 2 to the result and remove both the characters and solve the problem for the remaining subsequence.
+
+If both characters are different, then solve the problem twice, ignore the first character (keep the last character)and solve the remaining subsequence and 
+then ignore the last character (keep the first character) and solve it and pick the best out of two results
+
+
+Recursive Equations:
+
+LPS[i, i]	=	1	                                Every single character is a palindrome by itself of length 1
+LPS[i, j]	=	2	                                if j=i+1, sequence has only 2 characters
+LPS[i, j]	=	2 + LPS[i-1, j-1]	                If first and last characters are same
+LPS[i, j]	=	MAX(LPS[i+1,j], LPS[i, j-1])	    If first and last characters are not same
 
 */
 
@@ -30,13 +50,22 @@ public class Solution {
           
       int n = s.length();
       int [][] dp = new int[n][n];
-      
+        
       for(int i = 0; i< n; i++){
-          dp[i][i] = 1;   // Strings of length 1 are palindrome of lentgh 1
-          for(int j = i+1; j<len; j++){
-             if(s.charAt(i) == s.charAt(j)){
+          dp[i][i] = 1;  // Strings of length 1 are palindrome of lentgh 1
+      }    
+      
+      for(int sublen=2; sublen<=n; sublen++){          
+          for(int i=0;i<=n-sublen;i++){
+              
+             int j = i+sublen-1;
+             if(s.charAt(i) == s.charAt(j) && sublen==2){
+                 dp[i][j] = 2;
+             }
+             else if(s.charAt(i) == s.charAt(j)){
                 dp[i][j] = dp[i+1][j-1] + 2;
-             }else{
+             }
+             else{
                 dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
              }
            }
