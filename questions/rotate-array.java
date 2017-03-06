4e3c0acb3d4559f99b2a4c95f6d5aa26a8a2a652
virtual-life <pavithra03@gmail.com>
2017-03-05 19:26:59 -0800

    Update rotate-array.java

--- questions/rotate-array.java
+++ questions/rotate-array.java
@@ -5,8 +5,9 @@ Rotate array by number of positions
 public class Solution {
 	public ArrayList<Integer> rotateArray(ArrayList<Integer> A, int B) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
-		for (int i = 0; i < A.size(); i++) {
-			ret.add(A.get((i + B)%A.size()));
+		int n = A.size();
+		for (int i = 0; i < n; i++) {
+			ret.add(A.get((i + B)% n));
 		}
 		return ret;
 
