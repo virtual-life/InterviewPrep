7d5139a246ecb584f626bb7e7695bc4d11c71afb
virtual-life <pavithra03@gmail.com>
2017-03-08 16:38:38 -0800

    Create kth-smallest-element-in-a-BST.java

---
+++ questions/kth-smallest-element-in-a-BST.java
@@ -0,0 +1,45 @@
+/**
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+Note: 
+You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+Hint:
+
+Try to utilize the property of a BST.
+What if you could modify the BST node's structure?
+
+
+Time - O(h).
+
+*/
+
+public class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+    Stack<TreeNode> stack = new Stack<TreeNode>();
+ 
+    TreeNode p = root;
+    int result = 0;
+ 
+    while(!stack.isEmpty() || p!=null){
+        if(p!=null){
+            stack.push(p);
+            p = p.left;
+        }else{
+            TreeNode t = stack.pop();
+            k--;
+            if(k==0){
+                result = t.val;
+                break;
+            }
+            p = t.right;
+        }
+    }
+ 
+    return result;
+        
+    }
+}
