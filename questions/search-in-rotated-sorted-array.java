8b20b35f5bf0e75e57dd6b1c689be4428a331088
virtual-life <pavithra03@gmail.com>
2017-03-09 07:19:26 -0800

    Update search-in-rotated-sorted-array.java

--- questions/search-in-rotated-sorted-array.java
+++ questions/search-in-rotated-sorted-array.java
@@ -6,9 +6,11 @@
  You are given a target value to search. If found in the array return its index, otherwise return -1.
 
  You may assume no duplicate exists in the array.
+ 
+ Time - O(logn)
  */
 
-
+//Recursive
 public class Solution {
     public int search(int[] nums, int target) {
         return binarySearch(nums, 0, nums.length-1, target);
@@ -37,4 +39,66 @@ public class Solution {
             }
         }
     }
+}
+
+
+// iterative
+
+public int search(int[] nums, int target) {
+    int left = 0;
+    int right= nums.length-1;
+ 
+    while(left<=right){
+        int mid = left + (right-left)/2;
+        if(target==nums[mid])
+            return mid;
+ 
+        if(nums[left]<=nums[mid]){
+            if(nums[left]<=target&& target<nums[mid]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }else{
+            if(nums[mid]<target&& target<=nums[right]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }    
+    }
+ 
+    return -1;
+}
+
+
+// WIth duplicates 
+
+public boolean search(int[] nums, int target) {
+    int left=0;
+    int right=nums.length-1;
+ 
+    while(left<=right){
+        int mid = (left+right)/2;
+        if(nums[mid]==target)
+            return true;
+ 
+        if(nums[left]<nums[mid]){
+            if(nums[left]<=target&& target<nums[mid]){
+                right=mid-1;
+            }else{
+                left=mid+1;
+            }
+        }else if(nums[left]>nums[mid]){
+            if(nums[mid]<target&&target<=nums[right]){
+                left=mid+1;
+            }else{
+                right=mid-1;
+            }
+        }else{
+            left++;
+        }    
+    }
+ 
+    return false;
 }
