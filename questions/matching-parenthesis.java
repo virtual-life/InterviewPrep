2502ba6f85d8fa30faac0cc3b555d683360f60cb
virtual-life <pavithra03@gmail.com>
2017-04-01 10:42:36 -0700

    Update matching-parenthesis.java

--- questions/matching-parenthesis.java
+++ questions/matching-parenthesis.java
@@ -11,19 +11,18 @@ Space - O(1)
 */
 
 
-  public int getClosingParen(String sentence, int openingParenIndex) {
-    int openNestedParens = 0;
+  public int getClosingParen(String sentence, int index) {
+    int count = 0;
 
-    for (int position = openingParenIndex + 1; position < sentence.length(); position++) {
+    for (int position = index + 1; position < sentence.length(); position++) {
         char c = sentence.charAt(position);
 
         if (c == '(') {
-            openNestedParens += 1;
+            count ++;
         } else if (c == ')') {
-            if (openNestedParens == 0) {
+            count--;
+            if (count == 0) {
                 return position;
-            } else {
-                openNestedParens -= 1;
             }
         }
     }
