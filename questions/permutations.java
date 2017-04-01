3df427ffc74bc9958d45efb2e74a627f16c60d20
virtual-life <pavithra03@gmail.com>
2017-04-01 12:30:32 -0700

    Update permutations.java

--- questions/permutations.java
+++ questions/permutations.java
@@ -12,25 +12,23 @@ Time O(n! )
 */
 
 public class Solution {
-
-  public List<List<Integer>> permuteUnique(int[] nums) {
+    public List<List<Integer>> permuteUnique(int[] nums) {
         
-  ArrayList<List<Integer>> returnList = new ArrayList<List<Integer>>();
-	returnList.add(new ArrayList<Integer>());
+        ArrayList<List<Integer>> result = new ArrayList<List<Integer>>();
+	    result.add(new ArrayList<Integer>());
  
 	for (int i = 0; i < nums.length; i++) {
 		Set<ArrayList<Integer>> currentSet = new HashSet<ArrayList<Integer>>();
-		for (List<Integer> l : returnList) {
-			for (int j = 0; j < l.size() + 1; j++) {
-				l.add(j, nums[i]);
-				ArrayList<Integer> T = new ArrayList<Integer>(l);
-				l.remove(j);
-				currentSet.add(T);
+		
+		for (List<Integer> l : result) {
+			for (int j = 0; j < l.size()+1; j++) {			    
+				ArrayList<Integer> temp = new ArrayList<Integer>(l);
+				temp.add(j, nums[i]);
+				currentSet.add(temp);				
 			}
 		}
-		returnList = new ArrayList<List<Integer>>(currentSet);
-	}
- 
-	return returnList;
- }
+		result = new ArrayList<List<Integer>>(currentSet);
+	} 
+	return result;
+    }
 }
