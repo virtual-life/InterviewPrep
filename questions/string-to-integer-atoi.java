7f4beb3171ade6dd9e08ba6148390324b4554475
virtual-life <pavithra03@gmail.com>
2017-04-02 19:07:00 -0700

    Update string-to-integer-atoi.java

--- questions/string-to-integer-atoi.java
+++ questions/string-to-integer-atoi.java
@@ -21,7 +21,7 @@ public class Solution {
         double result = 0;
 
         // calculate value
-        while (str.length() > i && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
+        while (i < str.length() && str.charAt(i) >= '0' && str.charAt(i) <= '9') {
             result = result * 10 + (str.charAt(i) - '0');
             i++;
         }
