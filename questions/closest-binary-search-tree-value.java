e9532aeba97b66153b3cd7c155cd49a54c600492
virtual-life <pavithra03@gmail.com>
2018-11-11 16:44:00 -0800

    Update closest-binary-search-tree-value.java

--- questions/closest-binary-search-tree-value.java
+++ questions/closest-binary-search-tree-value.java
@@ -67,3 +67,50 @@ public class Solution {
         return result;
     }
 }
+
+/*
+    Find K closest nodes
+   
+*/
+
+// In- order traversal 
+// Time - O(n) Space O(k)
+
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        closestKValuesHelper(list, root, target, k);
+        return list;
+    }
+    
+    /**
+     * @return <code>true</code> if result is already found.
+     */
+    private boolean closestKValuesHelper(LinkedList<Integer> list, TreeNode root, double target, int k) {
+        
+        if (root == null) {
+            return false;
+        }
+        
+        if (closestKValuesHelper(list, root.left, target, k)) {
+            return true;
+        }
+        
+        if (list.size() == k) {
+            if (Math.abs(list.getFirst() - target) < Math.abs(root.val - target)) {
+                return true;
+            } else {
+                list.removeFirst();
+            }
+        }  
+        
+        list.addLast(root.val);
+        
+        
+        return closestKValuesHelper(list, root.right, target, k);
+    }
+}
+
+// Time - O(k log N )
+https://leetcode.com/problems/closest-binary-search-tree-value-ii/discuss/70503/O(logN)-Java-Solution-with-two-stacks-following-hint
+
