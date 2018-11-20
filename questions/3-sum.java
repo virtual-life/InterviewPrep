823f456a67ddddba83fa604e8d697052aea77fe5
virtual-life <pavithra03@gmail.com>
2018-11-20 10:35:53 -0800

    Update 3-sum.java

--- questions/3-sum.java
+++ questions/3-sum.java
@@ -60,6 +60,47 @@ public class Solution {
     }
 }
 
+/*
+ find one triplet 
+Time - O(n^2)
+*/
+
+class Solution { 
+  
+    // returns true if there is triplet with sum equal 
+    // to 'sum' present in A[]. Also, prints the triplet 
+    boolean find3Numbers(int A[], int arr_size, int sum) 
+    { 
+        int l, r; 
+  
+        /* Sort the elements */
+        Arrays.sort(A);
+  
+        /* Now fix the first element one by one and find the 
+           other two elements */
+        for (int i = 0; i < arr_size - 2; i++) { 
+  
+            // To find the other two elements, start two index variables 
+            // from two corners of the array and move them toward each 
+            // other 
+            l = i + 1; // index of the first element in the remaining elements 
+            r = arr_size - 1; // index of the last element 
+            while (l < r) { 
+                if (A[i] + A[l] + A[r] == sum) { 
+                    return true; 
+                } 
+                else if (A[i] + A[l] + A[r] < sum) 
+                    l++; 
+  
+                else // A[i] + A[l] + A[r] > sum 
+                    r--; 
+            } 
+        } 
+  
+        // If we reach here, then no triplet was found 
+        return false; 
+    } 
+
 
 /**
 3Sum closets
