aa356fff61653ea763d54736c9d2d3aa05dfbba3
virtual-life <pavithra03@gmail.com>
2019-02-12 01:09:21 -0800

    Create fruit-into-basket.java

---
+++ questions/fruit-into-basket.java
@@ -0,0 +1,79 @@
+
+/*
+In a row of trees, the i-th tree produces fruit with type tree[i].
+
+You start at any tree of your choice, then repeatedly perform the following steps:
+
+Add one piece of fruit from this tree to your baskets.  If you cannot, stop.
+Move to the next tree to the right of the current tree.  If there is no tree to the right, stop.
+Note that you do not have any choice after the initial choice of starting tree: you must perform step 1, then step 2, then back to step 1, then step 2, and so on until you stop.
+
+You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+
+What is the total amount of fruit you can collect with this procedure?
+
+ 
+
+Example 1:
+
+Input: [1,2,1]
+Output: 3
+Explanation: We can collect [1,2,1].
+Example 2:
+
+Input: [0,1,2,2]
+Output: 3
+Explanation: We can collect [1,2,2].
+If we started at the first tree, we would only collect [0, 1].
+Example 3:
+
+Input: [1,2,3,2,2]
+Output: 4
+Explanation: We can collect [2,3,2,2].
+If we started at the first tree, we would only collect [1, 2].
+Example 4:
+
+Input: [3,3,3,1,2,1,1,2,3,3,4]
+Output: 5
+Explanation: We can collect [1,2,1,1,2].
+If we started at the first tree or the eighth tree, we would only collect 4 fruits.
+
+
+Sliding Window 
+
+Time - O(n)
+Space - O(n)
+ 
+*/
+
+
+class Solution {
+    public int totalFruit(int[] tree) {
+        int result = 0, start = 0;
+        Counter count = new Counter();
+        for (int end = 0; end < tree.length; end++) {
+            count.add(tree[j], 1);
+            while (count.size() >= 3) {
+                count.add(tree[start], -1);
+                if (count.get(tree[start]) == 0)
+                    count.remove(tree[start]);
+                start++;
+            }
+
+            result = Math.max(result, end - start + 1);
+        }
+
+        return ans;
+    }
+}
+
+class Counter extends HashMap<Integer, Integer> {
+    public int get(int k) {
+        return containsKey(k) ? super.get(k) : 0;
+    }
+
+    public void add(int k, int v) {
+        put(k, get(k) + v);
+    }
+}
+
