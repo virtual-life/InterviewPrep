49e508bcd1056231f82d95f98e1ca2a50f3f771a
virtual-life <pavithra03@gmail.com>
2019-02-20 11:22:24 -0800

    Update edit-distance.java

--- questions/edit-distance.java
+++ questions/edit-distance.java
@@ -6,7 +6,7 @@ There are several cases to consider:
   -- If the len1 - len2 > 1, return false; 
   -- We compare each character of the two strings. if not equal. 
    -- If len1 > len2, we move i++, which means delete one character from string1, e.g. abc, ac
-   -- If len1 < len2, we move j++, which add one character for string1 (or delete one from string 2).
+   -- If len1 < len2, we move j++, which means add one character for string1 (or delete one from string 2).
   --  If len1 == len2, i++, j++
   
 */
@@ -68,12 +68,14 @@ public class Solution {
 
 /**
  Edit distance between 2 words  - http://professorjava.weebly.com/edit-distance.html
+ Time -  O(mn)
+ Space - O(mn)
 */
 
 /**
 It is important to note that there are only three possibilities one string can get to another: either deleting, inserting, or changing. 
 Thus, if we let dp(i,j) indicate the cost it takes from changing i to j, if s and t are the original strings, the cost is:
-Math.min(1+dp(i-1,j-1),1+dp(i,j-1),1+dp(i-1,j)) unless the last characters of the strings are the same, then the minimum is L(i-1,j-1). 
+Math.min(1+dp(i-1,j-1),1+dp(i,j-1),1+dp(i-1,j)) unless the last characters of the strings are the same, then the minimum is dp(i-1,j-1). 
 */
 
 public static int editDistance(String s1, String s2){
@@ -106,3 +108,32 @@ public static int editDistance(String s1, String s2){
      }
      return dp[s1.length()][s2.length()];
 }
+
+// Recursion 
+// https://www.geeksforgeeks.org/edit-distance-dp-5/
+
+static int editDist(String str1 , String str2 , int m ,int n) { 
+  
+    // If first string is empty, the only option is to 
+    // insert all characters of second string into first 
+    if (m == 0) return n; 
+       
+    // If second string is empty, the only option is to 
+    // remove all characters of first string 
+    if (n == 0) return m; 
+       
+    // If last characters of two strings are same, nothing 
+    // much to do. Ignore last characters and get count for 
+    // remaining strings. 
+    if (str1.charAt(m-1) == str2.charAt(n-1)) 
+        return editDist(str1, str2, m-1, n-1); 
+       
+    // If last characters are not same, consider all three 
+    // operations on last character of first string, recursively 
+    // compute minimum cost for all three operations and take 
+    // minimum of three values. 
+    return 1 + min ( editDist(str1,  str2, m, n-1),    // Insert  -> ab, abc 
+                     editDist(str1,  str2, m-1, n),   // Remove  -> abc, ab 
+                     editDist(str1,  str2, m-1, n-1) // Replace  -> ab, cd                     
+                   ); 
+    } 
