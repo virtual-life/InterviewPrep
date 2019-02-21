12a6bda0c71e903e3449932afcb0d52e4755fb08
virtual-life <pavithra03@gmail.com>
2019-02-21 02:38:21 -0800

    Update top-scores.java

--- questions/top-scores.java
+++ questions/top-scores.java
@@ -1,15 +1,16 @@
 /**
-You rank players in the game from highest to lowest score. So far you're using an algorithm that sorts in O(n\lg{n})O(nlgn) time, but players are complaining that their rankings aren't updated fast enough. You need a faster sorting algorithm.
+You rank players in the game from highest to lowest score. So far you're using an algorithm that sorts in O(nlgn) time, 
+but players are complaining that their rankings aren't updated fast enough. You need a faster sorting algorithm.
 
 Write a function that takes:
 
 an array of unsortedScores
 the highestPossibleScore in the game
-and returns a sorted array of scores in less than O(n\lg{n})O(nlgn) time.
+and returns a sorted array of scores in less than O(nlgn) time.
 
 For example:
 
-  int[] unsortedScores = {37, 89, 41, 65, 91, 53};
+int[] unsortedScores = {37, 89, 41, 65, 91, 53};
 final int HIGHEST_POSSIBLE_SCORE = 100;
 
 int[] sortedScores = sortScores(unsortedScores, HIGHEST_POSSIBLE_SCORE);
