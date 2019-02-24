36b129c94dd67e5d8fac8f45de41d756b579451d
virtual-life <pavithra03@gmail.com>
2019-02-24 13:17:34 -0800

    Update lfu-cache.java

--- questions/lfu-cache.java
+++ questions/lfu-cache.java
@@ -194,4 +194,67 @@ public class LFUCache {
             lowestFrequency = 0;
         }
     }
+  
+  
+  
+import java.util.*;
+  
+  // https://medium.com/@nanofaroque/lfu-cache-in-o-1-in-java-4bac0892bdb3
+
+public class LFUCache {
+    HashMap<Integer, Integer> vals;//cache K and V
+    HashMap<Integer, Integer> counts;//K and counters
+    HashMap<Integer, LinkedHashSet<Integer>> lists;//Counter and key list
+    int cap;
+    int min = -1;
+
+    public LFUCache(int capacity) {
+        cap = capacity;
+        vals = new HashMap<>();
+        counts = new HashMap<>();
+        lists = new HashMap<>();
+        lists.put(1, new LinkedHashSet<>());
+    }
+
+    public int get(int key) {
+        if (!vals.containsKey(key))
+            return -1;
+        // Get the count from counts map
+        int count = counts.get(key);
+        // increase the counter
+        counts.put(key, count + 1);
+        // remove the element from the counter to linkedhashset
+        lists.get(count).remove(key);
+
+        // when current min does not have any data, next one would be the min
+        if (count == min && lists.get(count).size() == 0)
+            min++;
+        if (!lists.containsKey(count + 1))
+            lists.put(count + 1, new LinkedHashSet<>());
+        lists.get(count + 1).add(key);
+        return vals.get(key);
+    }
+
+    public void set(int key, int value) {
+        if (cap <= 0)
+            return;
+        // If key does exist, we are returning from here
+        if (vals.containsKey(key)) {
+            vals.put(key, value);
+            get(key);
+            return;
+        }
+        if (vals.size() >= cap) {
+            int evit = lists.get(min).iterator().next();
+            lists.get(min).remove(evit);
+            vals.remove(evit);
+            counts.remove(evit);       
+        }
+        // If the key is new, insert the value and current min should be 1 of course
+        vals.put(key, value);
+        counts.put(key, 1);
+        min = 1;
+        lists.get(1).add(key);
+    }
+}
 }
