d406b7c25f22b137385172289c06acc7a2e6a24b
virtual-life <pavithra03@gmail.com>
2019-02-23 19:00:28 -0800

    Update palindrome-partitioning.java

--- questions/dynamic-programming/palindrome-partitioning.java
+++ questions/dynamic-programming/palindrome-partitioning.java
@@ -15,3 +15,30 @@ Output:
 Backtracking 
 
 */
+
+
+public List<List<String>> partition(String s) {
+   List<List<String>> result = new ArrayList<>();
+   backtrack(result, new ArrayList<>(), s, 0);
+   return result;
+}
+
+public void backtrack(List<List<String>> result, List<String> temp, String s, int start){
+   if(start == s.length())
+      result.add(new ArrayList<>(temp));
+   else{
+      for(int i = start; i < s.length(); i++){
+         if(isPalindrome(s, start, i)){
+            temp.add(s.substring(start, i + 1));
+            backtrack(result, temp, s, i + 1);
+            temp.remove(temp.size() - 1);
+         }
+      }
+   }
+}
+
+public boolean isPalindrome(String s, int low, int high){
+   while(low < high)
+      if(s.charAt(low++) != s.charAt(high--)) return false;
+   return true;
+} 
