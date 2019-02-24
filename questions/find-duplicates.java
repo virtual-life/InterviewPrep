50af9216a01907afe55a91668f7a3b45c55c9dfc
virtual-life <pavithra03@gmail.com>
2019-02-24 07:45:51 -0800

    Update find-duplicates.java

--- questions/find-duplicates.java
+++ questions/find-duplicates.java
@@ -2,8 +2,8 @@
 
 We have an array of integers, where:
 
-The integers are in the range 1..n1..n
-The array has a length of n+1n+1
+The integers are in the range 1..n
+The array has a length of n+1
 It follows that our array has at least one integer which appears at least twice. 
 But it may have several duplicates, and each duplicate may appear more than twice.
 
