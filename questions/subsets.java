27ec4532dda8b3c11a276fe3087416641880f174
virtual-life <pavithra03@gmail.com>
2019-02-23 18:35:17 -0800

    Update subsets.java

--- questions/subsets.java
+++ questions/subsets.java
@@ -16,6 +16,8 @@ If nums = [1,2,3], a solution is:
   [1,2],
   []
 ]
+
+
 */
 
 
@@ -54,5 +56,27 @@ public class Solution{
         result.add(new ArrayList<Integer>());
 
         return new ArrayList<ArrayList<Integer>>(result);
+    }
+}
+
+/*
+   Backtracking approach - very similar to DFS 
+*/
+
+public List<List<Integer>> subsets(int[] nums) {
+    List<List<Integer>> result = new ArrayList<>();
+    Arrays.sort(nums);
+    backtrack(result, new ArrayList<>(), nums, 0);
+    return result;
+}
+
+private void backtrack(List<List<Integer>> result , List<Integer> temp, int [] nums, int start){
+ 
+    result.add(new ArrayList<>(temp));
+ 
+    for(int i = start; i < nums.length; i++){
+        temp.add(nums[i]);
+        backtrack(result, temp, nums, i + 1);
+        temp.remove(temp.size() - 1);
     }
 }
