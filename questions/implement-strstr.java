c659fad406d012f0ae965a48e782762b66e55550
virtual-life <pavithra03@gmail.com>
2019-02-24 10:13:16 -0800

    Update implement-strstr.java

--- questions/implement-strstr.java
+++ questions/implement-strstr.java
@@ -18,8 +18,8 @@ public class Solution {
         }
 
         for (int i = 0; i < haystack.length() - needle.length() + 1; i++) {
-            int j;
-            for (j = 0; j < needle.length(); j++) {
+           
+            for (int j = 0; j < needle.length(); j++) {
                 if (haystack.charAt(i + j) != needle.charAt(j)) {
                     break;
                 }
