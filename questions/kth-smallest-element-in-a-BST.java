ee0bb4b694db4af0562cd19bd00b1460b338e95b
virtual-life <pavithra03@gmail.com>
2019-02-24 10:34:01 -0800

    Update kth-smallest-element-in-a-BST.java

--- questions/kth-smallest-element-in-a-BST.java
+++ questions/kth-smallest-element-in-a-BST.java
@@ -5,7 +5,8 @@ Note:
 You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
 
 Follow up:
-What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? 
+How would you optimize the kthSmallest routine?
 
 Hint:
 
